caccb1b9006e28333268d9f02c79ce58be0e0537
Elio <eliochen34@gmail.com>
2022-10-08 12:16:09 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express')
+const express = require('express') // 引用Express
 const app = express()
 const port = 3000
-const Todo = require('./models/todo')
+const Todo = require('./models/todo') // 引用/models路徑下的todo.js這個檔案
 
-const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser') // 引用body-parser，處理body
+app.use(bodyParser.urlencoded({ extended: true })) // 規定每一筆請求都要經過body-parser進行前置處理
+
+const exphbs = require('express-handlebars') // 引用express-hbs
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // 引用mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -26,6 +29,17 @@ app.get('/', (req, res) => {
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
