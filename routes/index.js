99e4549f869cdbf3781f878983d2e86f502c3cbd
Elio <eliochen34@gmail.com>
2022-10-24 13:26:43 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ const router = express.Router()
 
 // 引入home模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合 / 字串的request導向home模組
-router.use('/', home)
-
 // 引入todos模組程式碼
 const todos = require('./modules/todos')
+const users = require('./modules/users')
+
+
+// 將網址結構符合 / 字串的request導向home模組
+router.use('/', home)
 // 將網址結構符合 /todos字串開頭的request導向todos模組
 router.use('/todos', todos)
-
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
