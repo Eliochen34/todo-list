a974af8e28e047b252d21c6c1502a7c41c1ffc96
Elio <eliochen34@gmail.com>
2022-10-24 16:58:59 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express') // 引用Express
 const exphbs = require('express-handlebars') // 引用express-hbs
 const bodyParser = require('body-parser') // 引用body-parser，處理body
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes') // 掛載路由器
 require('./config/mongoose')
@@ -11,6 +12,12 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: "ThisIsMySecret",
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true })) // 規定每一筆請求都要經過body-parser進行前置處理
 app.use(methodOverride('_method'))
 
