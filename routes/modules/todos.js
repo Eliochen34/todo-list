37cb4b7b6729e5fb496c9bf30383d8393dfb8d1e
Elio <eliochen34@gmail.com>
2022-10-25 17:13:51 +0800

    feat: add relations between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,43 +8,48 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const name = req.body.name
-  return Todo.create({ name })
+  const userId = req.user._id
+  return Todo.create({ name, userId })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(err => console.log(err))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .lean()
     .then(todo => res.render('edit', { todo }))
     .catch(err => console.log(err))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const _id = req.params.id
+  const userId = req.user._id
   const { name, isDone } = req.body
-  return Todo.findById(id)
+  return Todo.findOne({ _id, userId })
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect('/'))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch(err => console.log(err))
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Todo.findOne({ _id, userId })
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
